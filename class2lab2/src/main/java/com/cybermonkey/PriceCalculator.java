9cdfc0aba296d3012e6f9a696a0f8433589e455a
hokinfai <hokinfaibb@gmail.com>
2020-10-03 22:29:50 +0800

    add class 4 lab

--- class2lab2/src/main/java/com/cybermonkey/PriceCalculator.java
+++ class2lab2/src/main/java/com/cybermonkey/PriceCalculator.java
@@ -5,13 +5,13 @@ import java.math.RoundingMode;
 
 public class PriceCalculator {
 
-    public int getNumberOfProducts(int productId, double amount) {
+    public static int getNumberOfProducts(int productId, double amount) {
         double productPrice = DrinkCategory.getProductPrice(productId);
 
         return Double.valueOf(amount / productPrice).intValue();
     }
 
-    public double getChanges(int productId, double amount) {
+    public static double getChanges(int productId, double amount) {
         double productPrice = DrinkCategory.getProductPrice(productId);
         double changes = amount % productPrice;
         return new BigDecimal(changes).setScale(2, RoundingMode.HALF_DOWN).doubleValue();
--- class2lab2/src/main/java/com/cybermonkey/VendingMachineApplication.java
+++ class2lab2/src/main/java/com/cybermonkey/VendingMachineApplication.java
@@ -2,10 +2,12 @@ package com.cybermonkey;
 
 import java.util.Scanner;
 
+import static com.cybermonkey.PriceCalculator.getChanges;
+import static com.cybermonkey.PriceCalculator.getNumberOfProducts;
+
 public class VendingMachineApplication {
 
     public static void main(String[] args) {
-        PriceCalculator calculator = new PriceCalculator();
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please select your drink: 1. Coke, 2. Sport Drink");
@@ -16,8 +18,8 @@ public class VendingMachineApplication {
         System.out.print("How much you will pay: ");
         double amount = scanner.nextDouble();
 
-        int numberOfProducts = calculator.getNumberOfProducts(id, amount);
-        double changes = calculator.getChanges(id, amount);
+        int numberOfProducts = getNumberOfProducts(id, amount);
+        double changes = getChanges(id, amount);
 
         System.out.println("\nYou can buy " + numberOfProducts + " " + DrinkCategory.getDrinkCategory(id));
         System.out.println("Your changes are " + changes);
