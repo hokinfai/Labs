61146d336b280e4af0c99de6c244e105c3b02e2d
hokinfai <hokinfaibb@gmail.com>
2020-10-09 15:21:46 +0800

    added class 3 lab 2

---
+++ class3lab2/src/main/java/com/cybermonkey/Employee.java
@@ -0,0 +1,39 @@
+package com.cybermonkey;
+
+public class Employee {
+    private String name;
+    private double numberOfHoursPerDay;
+    private double salary;
+
+    public Employee(String name, double numberOfHoursPerDay, double salary) {
+        this.name = name;
+        this.numberOfHoursPerDay = numberOfHoursPerDay;
+        this.salary = salary;
+    }
+
+    private void addSal() {
+        if (salary < 500.00) {
+            salary = salary + 10.0;
+        }
+    }
+
+    private void addWork() {
+        if (numberOfHoursPerDay > 6.0) {
+            salary = salary + 5.0;
+        }
+    }
+
+    public double getSalary() {
+        addWork();
+        addSal();
+        return salary;
+    }
+
+    public String getInfo() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", numberOfHoursPerDay=" + numberOfHoursPerDay +
+                ", salary=" + salary +
+                '}';
+    }
+}
